442c51075ccae08a766f65604cb2e1df33f30225
Oscar Gomes dos Santos Junior <oscar.gomes.junior@hotmail.com>
2020-03-20 06:33:42 -0300

    Correção BD

--- app/Http/Controllers/TelefoneController.php
+++ app/Http/Controllers/TelefoneController.php
@@ -1,15 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use DB;
+use Illuminate\Support\Facades\DB as FacadesDB;
 
 class TelefoneController extends Controller
 {
     public function listarTelefones($empresa_id){
         $empresaId = $empresa_id;
-        $numeros = DB::table('telefones')->where('empresa_id', $empresa_id)->get();
+        $numeros = FacadesDB::table('telefones')->where('empresa_id', $empresa_id)->get();
         return $numeros;
     }
 }
